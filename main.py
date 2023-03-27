09762255a86133dbebf5b895413c1cad228aea7b
aleksmn <al-mn@ya.ru>
2023-03-27 19:47:28 +0000

    add HTML and CSS

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Привет, это Flask</p>"
+def index():
+  # return "<p>Привет, это Flask</p>"
+  return render_template('index.html')
+  
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
   
