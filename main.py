6e3e2e302158dd550f0fe8c9f66f34c374239a3f
aleksmn <>
2023-04-03 19:26:12 +0000

    added dynamic data

--- main.py
+++ main.py
@@ -2,14 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+projects = [{
+  'id': 1,
+  'title': "Калькулятор метрических величин",
+  'description': 'Программа для перевода одной физической феличины в другую',
+  'image': 'screen_1.png',
+  'techs': 'Python, tkinter, customtkinter'
+}, {
+  'id': 2,
+  'title': 'Таймер',
+  'description': 'Обратный отсчет времени',
+  'image': 'screen_2.png',
+}, {
+  'id': 3,
+  'title': 'Домашняя библиотека',
+  'description': 'Каталог книг с базой данных',
+  'image': 'screen_3.png',
+  'techs': 'Python, tkinter, customtkinter, sqlite',
+}]
+
 
 @app.route("/")
 def index():
   # return "<p>Привет, это Flask</p>"
-  return render_template('index.html')
-  
+  return render_template('index.html', projects=projects)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
-  
+  app.run(host='0.0.0.0', debug=True)
